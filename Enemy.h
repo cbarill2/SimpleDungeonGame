3a330bba38489f83eee87cf8dec8ad2ba6dfe989
Chris Barill <cbarill2@mix.wvu.edu>
2021-01-19 00:13:36 -0600

    Update Enemy.h
    Need default constructor

--- Enemy.h
+++ Enemy.h
@@ -7,6 +7,7 @@ class Enemy : public Unit
 {
 private:
 public:
+    Enemy() : Unit{} {}
     Enemy(int x, int y, sf::Texture &activeTexture, sf::Texture &inactiveTexture)
         : Unit{x, y, activeTexture, inactiveTexture, false} {}
 };
